acbd49cb07b9d7da58cbd7ae40474c78430d9d9c
Abi <58501537+abdullah1509@users.noreply.github.com>
2023-09-21 23:11:24 +0530

    Add files via upload

---
+++ chatgpt.py
@@ -0,0 +1,56 @@
+import os
+from langchain.vectorstores.chroma import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import ConversationalRetrievalChain
+from langchain.schema import HumanMessage, AIMessage
+from dotenv import load_dotenv
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+def make_chain():
+    model = ChatOpenAI(
+        model_name="gpt-3.5-turbo",
+        temperature="0",
+        # verbose=True
+    )
+    embedding = OpenAIEmbeddings()
+
+    vector_store = Chroma(
+        collection_name="Four-Steps-to-Forgiveness",
+        embedding_function=embedding,
+        persist_directory="chroma",
+    )
+
+    return ConversationalRetrievalChain.from_llm(
+        model,
+        retriever=vector_store.as_retriever(),
+        return_source_documents=True,
+        # verbose=True,
+    )
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    chain = make_chain()
+    chat_history = []
+
+    while True:
+        print()
+        question = input("Question: ")
+
+        # Generate answer
+        response = chain({"question": question, "chat_history": chat_history})
+
+        # Retrieve answer
+        answer = response["answer"]
+        source = response["source_documents"]
+        chat_history.append(HumanMessage(content=question))
+        chat_history.append(AIMessage(content=answer))
+
+        # Display answer
+        for document in source:
+            print(f"Page: {document.metadata['page_number']}")
+            print(f"Text chunk: {document.page_content[:200]}...\n")
+        print(f"Answer: {answer}")
